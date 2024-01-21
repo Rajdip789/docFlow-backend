f0e47e20b148a9766150912aecaf9fa13c6d53cb
rajdip <palrajdip33@gmail.com>
2024-01-21 15:17:19 +0530

    chat model modified and dev dependency updated

--- models/chatModel.js
+++ models/chatModel.js
@@ -5,7 +5,7 @@ const ChatSchema = new mongoose.Schema(
 		messages: [
 			{
 				user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
-				username: { type: String, required: true, unique: true },
+				username: { type: String, required: true },
 				msg: { type: String, required: true },
 				createdAt: { type: Date, default: Date.now, required: true }
 			}
